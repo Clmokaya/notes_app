de4d10c2a35ea6a0641efd9120314c6b83d2f761
clmokaya <mokmakori@gmail.com>
2025-03-25 11:46:45 +0300

    initial commit

---
+++ android/app/src/main/kotlin/com/cmmapps/notes_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.cmmapps.notes_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/models/note_database.dart
@@ -0,0 +1,11 @@
+class NoteDatabase {
+  // INITIALIZE DATABASE
+
+  //CREATE - A note and save to db
+
+  //READ- notes from db
+
+  //UPDATE - a note in db
+
+  //DELETE - a note from db
+}
