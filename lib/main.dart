6099a521f8795b18df76ab31c069846d42f2b594
clmokaya <mokmakori@gmail.com>
2025-04-15 16:04:41 +0300

    ui

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       home: NotesPage(),
       theme: Provider.of<Themeprovider>(context).themeData,
     );
